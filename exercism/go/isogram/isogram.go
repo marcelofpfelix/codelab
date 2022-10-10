15c4b938f884b0def5bd0b7142042e30155159be
Marcelo Félix <marcelofpfelix@gmail.com>
2022-10-10 19:53:38 +0100

    code of the day

--- exercism/go/isogram/isogram.go
+++ exercism/go/isogram/isogram.go
@@ -1,5 +1,20 @@
 package isogram
 
+import (
+	"strings"
+	"unicode"
+)
+
 func IsIsogram(word string) bool {
-	panic("Please implement the IsIsogram function")
+	word = strings.ToLower(word)
+	for i, c := range word {
+		// if char c is alpha and if char is duplicated
+		if unicode.IsLetter(c) && strings.ContainsRune(word[i+1:], c) {
+			return false
+		}
+	}
+	return true
 }
+
+// ContainsRune used to check the given string contains the specified rune or not in it.
+// IsLetter returns true if rune r is a letter
