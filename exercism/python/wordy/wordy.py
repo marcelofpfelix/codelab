94774426be47cf54dd595a101110de4da0e2fd1a
Marcelo Félix <marcelofpfelix@gmail.com>
2022-12-20 23:19:16 +0000

    code of the day

--- exercism/python/wordy/wordy.py
+++ exercism/python/wordy/wordy.py
@@ -2,7 +2,36 @@
 wordy.py
 """
 
-#from ast import literal_eval
+import ast
+import operator as op
+
+# supported operators
+operators = {ast.Add: op.add, ast.Sub: op.sub, ast.Mult: op.mul,
+             ast.Div: op.truediv, ast.Pow: op.pow, ast.BitXor: op.xor,
+             ast.USub: op.neg}
+
+def eval_(node):
+    """
+    eval for arithmeticsas in simpleeval
+    """
+    # <number>
+    if isinstance(node, ast.Num):
+        return node.n
+    # <left> <operator> <right>
+    if isinstance(node, ast.BinOp):
+        return operators[type(node.op)](eval_(node.left), eval_(node.right))
+    # <operator> <operand> e.g., -1
+    if isinstance(node, ast.UnaryOp):
+        return operators[type(node.op)](eval_(node.operand))
+    raise TypeError(node)
+
+
+def unk_op(question):
+    """
+    simple generator to check if the operation is known
+    """
+    for value in question:
+        yield value.isalpha() and value not in ("What", "is")
 
 
 def answer(question):
@@ -13,16 +42,18 @@ def answer(question):
     if not question.startswith('What is'):
         raise ValueError("unknown operation")
 
-    question = question.replace("What is ","").replace("?","").replace("?","").replace("plus", "+").replace("multiplied by","*").replace("minus", "-").replace("divided by", "/").split()
+    question = question.replace("What is ", "").replace("?", "").replace("?", "")
+    question = question.replace("plus", "+").replace("multiplied by", "*").replace("minus", "-")
+    question = question.replace("divided by", "/").split()
 
     # this should be replaced with a loop
     question.insert(0, "(")
     question.insert(4, ")")
 
-    if any([x.isalpha() for x in question if x not in ("What", "is")]):
+    if any(list(unk_op(question))):
         raise ValueError("unknown operation")
 
     try:
-        return eval(" ".join(question))
-    except:
-        raise ValueError("syntax error")
+        return eval_(ast.parse(" ".join(question), mode='eval').body)
+    except Exception as exception:
+        raise ValueError("syntax error") from exception
