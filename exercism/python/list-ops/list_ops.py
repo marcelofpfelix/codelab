119ea99296458c7c6b7a45e300377e9bb6dabac2
Marcelo Félix <marcelofpfelix@gmail.com>
2023-01-09 22:17:09 +0000

    code of the day: python/list-ops

--- exercism/python/list-ops/list_ops.py
+++ exercism/python/list-ops/list_ops.py
@@ -1,30 +1,68 @@
+"""
+List Ops
+"""
+
+
 def append(list1, list2):
+    """
+    `append` (*given two lists, add all items in the second list to the end of the first list*);
+    """
     return list1 + list2
 
 
 def concat(lists):
+    """
+    `concatenate` (*given a series of lists, combine all items in all lists
+        into one flattened list*);
+    """
     return [item for sublist in lists for item in sublist]
 
 
 def filter(function, list):
-    pass
+    """
+    `filter` (*given a predicate and a list, return the list of all items for
+        which `predicate(item)` is True*);
+    """
+    return [item for item in list if function(item)]
 
 
 def length(list):
-    pass
+    """
+    `length` (*given a list, return the total number of items within it*);
+    """
+    return len(list)
 
 
 def map(function, list):
-    pass
+    """
+    `map` (*given a function and a list, return the list of the results of
+        applying `function(item)` on all items*);
+    """
+    return [function(item) for item in list]
 
 
 def foldl(function, list, initial):
-    pass
+    """
+    `foldl` (*given a function, a list, and initial accumulator, fold (reduce)
+        each item into the accumulator from the left using `function(accumulator, item)`*);
+    """
+    for item in list:
+        initial = function(initial, item)
+    return initial
 
 
 def foldr(function, list, initial):
-    pass
+    """
+    `foldr` (*given a function, a list, and an initial accumulator, fold (reduce)
+        each item into the accumulator from the right using `function(item, accumulator)`*);
+    """
+    for item in list[::-1]:
+        initial = function(item, initial)
+    return initial
 
 
 def reverse(list):
-    pass
+    """
+    `reverse` (*given a list, return a list with all the original items, but in reversed order*);
+    """
+    return list[::-1]
